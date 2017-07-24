230eca005c6219c3d24cd1c38a216827e256dcc9
root <313690636@qq.com>
2017-07-24 23:18:06 +0800

    联系qq

--- src/view/web/default.php
+++ src/view/web/default.php
@@ -10,6 +10,7 @@
         <a href="http://www.cnblogs.com/echosong/">my blog</a> /
         Contact me:
         <a class="email" href="mailto:313690636@qq.com">313690636@qq.com</a>
-    </div>
+        <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=313690636&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:313690636:51" alt="es" title="es"/></a>
+       </div>
 </footer>
 <div class="backdrop"></div>
