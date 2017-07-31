4fea283e5f99d6bacba992a63f5015ef6991091a
17717353366 <songfeigang@dianbaobao.com>
2017-07-31 09:21:38 +0800

    配置文件修改

--- src/config.php
+++ src/config.php
@@ -1,41 +1,7 @@
 <?php
-
-$config = array(
-    'rewrite' => array(
-        //设置模块 碰到 http://{host}/admin/ 认为进入了后台模块 数组 0 标识默认 m
-        'm' => ['web', 'admin', 'app', 'api'],
-        'c' => 'main', //controller 默认值
-        'a' => 'index', //action 默认值,
-        'isRewrite' => true //是否开启伪静态 .htaccess 文件配置
-    ),
-    'debug' => true,
-    'plugins' => ['include', 'plugin'], //扩展目录
-    'static' => "res",
-    'logPath' => 'logs' //日志路径，请保证路径权限可写
-);
-
-$dbb = array(
-    'mysql' => [
-        //主库
-        'master' => [
-            'MYSQL_HOST' => '127.0.0.1',
-            'MYSQL_PORT' => '3306',
-            'MYSQL_USER' => 'root',
-            'MYSQL_DB' => 'db_demo',
-            'MYSQL_PASS' => '123456',
-            'MYSQL_CHARSET' => 'utf8',
-        ],
-        //从库可以加入多个实例
-        'slave' => [
-            'MYSQL_HOST' => '127.0.0.1',
-            'MYSQL_PORT' => '3306',
-            'MYSQL_USER' => 'root',
-            'MYSQL_DB' => 'db_demo',
-            'MYSQL_PASS' => '123456',
-            'MYSQL_CHARSET' => 'utf8',
-        ]
-    ],
-    'prefix' => 'mo_',
-);
-
-return $dbb + $config;
+/**
+ * Created by PhpStorm.
+ * User: echosong
+ * Date: 2017/7/31
+ * Time: 9:20
+ */
